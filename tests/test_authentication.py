6464e3e6e1c71c968834b3fcab6adeb541c5c14b
kigill <kyrylo.karnyshov@gmail.com>
2019-08-13 22:22:26 +0300

    Test of basic auth & logout

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -8,6 +8,17 @@ import pytest
 
 def test_ping_login():
     r = requests.get("https://the-internet.herokuapp.com/login")
-    cookie = r.cookies
-    print(cookie)
+#    cookie = r.cookies
+#    print(cookie)
     assert 200 == r.status_code
+
+
+def test_auth():
+    r = requests.post("https://the-internet.herokuapp.com/authenticate",
+                      {'username': 'tomsmith', 'password': 'SuperSecretPassword!'})
+    cookie = r.cookies
+    assert True == ("Welcome to the Secure Area." in r.text)
+
+    d = requests.get('https://the-internet.herokuapp.com/logout', cookies=cookie)
+    assert 200 == d.status_code
+    print(d.text)
