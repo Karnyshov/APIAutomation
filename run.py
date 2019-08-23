e38b42fa0615020455a38d1b2e79ce942b9b3571
Vlad Davidchenko <Vlad Davidchenko>
2019-08-24 00:01:39 +0300

    1. add class TestAuth 2. add conftest 3. remove pytest.ini 4. add run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+
+
+def extend_suite(cases):
+    for case in cases:
+        suite.addTest(case)
+
+
+suite = unittest.TestSuite()
+
+if int(os.getenv('reqres', '1')) == 1:
+    from tests.test_authentication import TestAuth
+
+    extend_suite([TestAuth])
+
+
+def run():
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,29 +1,28 @@
 from utils.Api import Api
 
 
-def test_ping_login():
-    api = Api()
-    response = api.get(api.LOGIN_URL)
-    assert 200 == response.status_code
-    print(response.text)
-    assert True == ("This is where you can log into the secure area." in response.text)
+class TestAuth:
+    def test_ping_login(self):
+        api = Api()
+        response = api.get(api.LOGIN_URL)
+        assert 200 == response.status_code
+        print(response.text)
+        assert True == ("This is where you can log into the secure area." in response.text)
 
+    def test_auth_new(self):
+        api = Api()
+        response = api.login()
 
-def test_auth_new():
-    api = Api()
-    response = api.login()
+        print('-->', api.getCookies())
+        print(response.text)
+        print(response.status_code)
+        assert True == ("Welcome to the Secure Area." in response.text)
 
-    print('-->', api.getCookies())
-    print(response.text)
-    print(response.status_code)
-    assert True == ("Welcome to the Secure Area." in response.text)
+    def test_logout(self):
+        api = Api()
+        api.login()
+        response = api.get(api.LOGOUT_URL)
 
-
-def test_logout():
-    api = Api()
-    api.login()
-    response = api.get(api.LOGOUT_URL)
-
-    assert 200 == response.status_code
-    assert True == ("This is where you can log into the secure area." in response.text)
-    print(response.text)
+        assert 200 == response.status_code
+        assert True == ("This is where you can log into the secure area." in response.text)
+        print(response.text)
