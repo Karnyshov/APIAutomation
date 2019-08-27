ae4526c576af9d5a3090bdb1acb4551dfcc51d37
kyrylo.karnyshov@gmail.com <Hub0ctObus1>
2019-08-27 23:40:07 +0300

    Remove unnecessary prints for report generation

--- conftest.py
+++ conftest.py
@@ -9,8 +9,3 @@ def authentication():
 
     assert 200 == response.status_code
     assert True == ("Welcome to the Secure Area." in response.text)
-
-    print("\n")
-    print(response.status_code)
-    print("\n")
-    print(response.text)
--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -6,18 +6,14 @@ class TestFlow(Template):
     def test_ping_login(self):
         response = self.api.get(self.api.LOGIN_URL)
         assert 200 == response.status_code
-        print(response.text)
         assert True == ("This is where you can log into the secure area." in response.text)
 
     def test_auth_new(self):
         response = self.api.login()
-        print('-->', self.api.getCookies())
-        print(response.text)
-        print(response.status_code)
+        assert 200 == response.status_code
         assert True == ("Welcome to the Secure Area." in response.text)
 
     def test_logout_new(self, authentication):
         response = self.api.get(self.api.LOGOUT_URL)
         assert 200 == response.status_code
         assert True == ("This is where you can log into the secure area." in response.text)
-        print(response.text)
