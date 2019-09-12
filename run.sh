5d11a6983bcd7d7fb72c203c8d2f083a71aa2cc3
Kyrylo <kyrylo.karnyshov@gmail.com>
2019-09-12 18:15:11 +0300

    Add run scripts for Jenkins "Free Form Project"

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PYENV_HOME=$WORKSPACE/.pyenv/
+virtualenv --no-site-packages "${PYENV_HOME}"
+source "${PYENV_HOME}"/bin/activate
+pip install -U pytest
+pip install -r requirements.txt
+pytest test_authentication.py
+deactivate
