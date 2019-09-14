a77202991e94617ccbdda2e2c7daa0eb0941dac7
kyrylo.karnyshov@gmail.com <Hub0ctObus1>
2019-09-14 20:18:03 +0300

    modify scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 PYENV_HOME=$WORKSPACE/.pyenv/
-virtualenv --no-site-packages "${PYENV_HOME}"
-source "${PYENV_HOME}"/bin/activate
-pip install -U pytest
+virtualenv --no-site-packages $PYENV_HOME
+source $PYENV_HOME/bin/activate
 pip install -r requirements.txt
 pytest test_authentication.py
 deactivate
