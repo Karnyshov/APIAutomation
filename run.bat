f25e5ab346a8fef5f4ee5cd7f2a4dc6552018ad1
kyrylo.karnyshov@gmail.com <Hub0ctObus1>
2019-09-14 19:37:38 +0300

    Update bat file

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
-PYENV_HOME=$WORKSPACE/.pyenv/
-virtualenv --no-site-packages %PYENV_HOME%
-source %PYENV_HOME}%/bin/activate
-pip install -U pytest
+PYENV_HOME=$WORKSPACE\.pyenv\
+venv  my_env --no-site-packages %PYENV_HOME%
+source %PYENV_HOME%\Scripts\activate.bat
 pip install -r requirements.txt
 pytest test_authentication.py
 deactivate
