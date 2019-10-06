1002ed462943146065ff25cef20b05b217a4a5d2
kyrylo.karnyshov@gmail.com <Hub0ctObus1>
2019-10-06 21:11:32 +0300

    Remove .bat file Add parallel execution & allure reporting Update .sh file for TeamCity

--- run.bat
+++
@@ -1,7 +0,0 @@
-mkdir pyenv
-PYENV_HOME=$WORKSPACE\pyenv\
-venv my_env --no-site-packages %PYENV_HOME%
-source %PYENV_HOME%\Scripts\activate.bat
-pip install -r requirements.txt
-pytest test_authentication.py
-deactivate
--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#TeamCity
 
-PYENV_HOME=$WORKSPACE/.pyenv/
-virtualenv --no-site-packages $PYENV_HOME
+pip install virtualenv
+venv --no-site-packages TestEnv
+PYENV_HOME=/TestEnv
 source $PYENV_HOME/bin/activate
 pip install -r requirements.txt
 pytest test_authentication.py
